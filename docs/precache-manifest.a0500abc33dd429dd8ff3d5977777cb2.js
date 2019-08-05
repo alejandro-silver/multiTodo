6dc44822e68c46547493e9210c81f607996b696b
alejandro-silver <fleacr3@gmail.com>
2019-08-05 19:23:00 +0700

    docs3

---
+++ docs/precache-manifest.a0500abc33dd429dd8ff3d5977777cb2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43d793d8c6af44aaabefdaa21cb2ecb4",
+    "url": "/MultiTodo/index.html"
+  },
+  {
+    "revision": "d48fbdfb8776549ec908",
+    "url": "/MultiTodo/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "3774559311ce219267a0",
+    "url": "/MultiTodo/static/js/2.ae4fd101.chunk.js"
+  },
+  {
+    "revision": "d48fbdfb8776549ec908",
+    "url": "/MultiTodo/static/js/main.3b00468c.chunk.js"
+  },
+  {
+    "revision": "fe04f17dd1f925534806",
+    "url": "/MultiTodo/static/js/runtime~main.74fb049b.js"
+  }
+]);
